90aa961b867b1e9b29a98d5dedad358446da8129
Danilo Arantes <daniloarantes_00@hotmail.com>
2018-08-24 01:08:33 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nelioalves/cursospring/CursospringApplication.java
+++ src/main/java/com/nelioalves/cursospring/CursospringApplication.java
@@ -1,12 +1,30 @@
 package com.nelioalves.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.nelioalves.cursospring.domain.Categoria;
+import com.nelioalves.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }

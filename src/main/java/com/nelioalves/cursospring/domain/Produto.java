89f61c8dcc5347cb52e8efc9a2193f6a935cbb51
Danilo Arantes <daniloarantes_00@hotmail.com>
2018-08-28 02:29:53 -0300

    ItemPedido e ItemPedidoPK Parte 1

--- src/main/java/com/nelioalves/cursospring/domain/Produto.java
+++ src/main/java/com/nelioalves/cursospring/domain/Produto.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursospring.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +35,8 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 	}	
 	
@@ -42,6 +46,15 @@ public class Produto implements Serializable{
 		this.nome = nome;
 		this.preco = preco;
 	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens)
+		  lista.add(x.getPedido());
+		
+		return lista;
+	}
+	
 	
 	@Override
 	public int hashCode() {
@@ -98,6 +111,14 @@ public class Produto implements Serializable{
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 	
 	

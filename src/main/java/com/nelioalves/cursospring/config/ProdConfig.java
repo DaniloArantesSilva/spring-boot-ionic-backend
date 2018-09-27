6915ec6a6be740f520904c0da63d6ca955b00fef
Danilo Arantes <daniloarantes_00@hotmail.com>
2018-09-26 23:02:17 -0300

    Ajustes adicionais

---
+++ src/main/java/com/nelioalves/cursospring/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package com.nelioalves.cursospring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nelioalves.cursospring.services.DBService;
+import com.nelioalves.cursospring.services.EmailService;
+import com.nelioalves.cursospring.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!strategy.equals("create"))
+			return false;
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
